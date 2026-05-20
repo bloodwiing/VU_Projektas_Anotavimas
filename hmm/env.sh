1bd3d6e0c7f0314139fe03db7a341ad60f93b4da
Donatas Kirda <bloodwiing+git@alchemix.dev>
2026-05-20 05:59:49 +0300

    HTKGraph project

--- hmm/env.sh
+++
@@ -1,4 +0,0 @@
-PATH=$PATH:/mnt/d/Uni/Bachelors/CourseWork/HTK-3.5-beta/htk/HTKTools
-PATH=$PATH:/mnt/d/Uni/Bachelors/CourseWork/anotavimas/hmm/tools
-
-alias PHVite="python /mnt/d/Uni/Bachelors/CourseWork/anotavimas/hmm/tools/PHVite.py"
